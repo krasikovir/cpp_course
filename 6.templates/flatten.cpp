ae48426928baf06be7cad3a2b3afcd73b182a90a
Igor Krasikov <krasikovir@Igors-MacBook-Air.local>
2022-08-25 19:34:40 +0300

    section 6

---
+++ 6.templates/flatten.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template <typename Type>
+void flatten(const Array<Type>& array, std::ostream& out)
+{
+for (size_t i=0; i != array.size(); ++i){
+out << array[i] << " ";
+}
+}
+template <typename Type>
+void flatten(const Array< Array<Type> >& array, std::ostream& out)
+{
+for (size_t i=0; i != array.size(); ++i){
+flatten(array[i], out);
+}
+}
